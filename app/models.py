84015cdd127aa549a8a42872a3955df98cb3ca11
beth nduta <bethnduta05@gmail.com>
2022-05-02 23:26:26 +0300

    made new files

--- app/models.py
+++ app/models.py
@@ -1,10 +1,23 @@
-class News:
+class Articles:
     '''
-    news class to define news object
+    news articles class to define news object
     '''
-def __init__(self,title,description,urlToImage,content,publishedAt):
+def __init__(self,title,description,urlToImage,content,publishedAt,url):
     self.title = title
     self.description = description
     self.urlToImage = urlToImage
     self.content = content
     self.publishedAt = publishedAt
+    self.url = url
+
+
+class Sources:
+    '''
+    news sources class that defines the origin of the news
+    '''
+
+    def __init__(self, id, name, category,description):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.description = description
