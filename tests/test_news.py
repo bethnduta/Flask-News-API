480f88b8186cc49bbb386e2a5125b87c8c4a633b
beth nduta <bethnduta05@gmail.com>
2022-05-04 10:33:17 +0300

    test.py

--- tests/test_news.py
+++ tests/test_news.py
@@ -1,9 +1,15 @@
 import unittest
-from app.models import Article, Sources
+from app.models import Articles, Sources
 
-class ArticleTest(unittest.TestCase):
+class ArticlesTest(unittest.TestCase):
     '''
     Test class to test the behaviour of the articles
     '''
     def setUp(self):
-        
+        '''
+        setUp method to run before the test
+        '''
+        self.new_article = Articles("abc","Beth","test_title","test_description")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article, Articles))    
